5681fe8247fc929f0247a2a18549398f8dea7599
caspencer <cas628@gmail.com>
2012-05-02 14:12:50 -0400

    initial commit

---
+++ TestClass1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xunit;
+
+namespace $safeprojectname$
+{
+    public class TestClass1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
